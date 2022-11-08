5644bf6c7b1bccd08eb18ed93ddcffe742c208b7
viololahe <65051338+viololahe@users.noreply.github.com>
2022-11-07 22:52:38 -0500

    initial

---
+++ script.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById('button1');
+
+function buttonClick(){
+    btn.innerHTML = "Paragraph changed";
+    console.log('clicked');
+}
+
+// button1.addEventListener('click', function(){
+//       console.log('click');
+//     })
+
+// function clickEvent(){
+//     console.log('click');
+// }
